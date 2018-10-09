e8d3b4ac8706b6a5e1e8b381255b472ab03e6e28
ben <pinel.benjamin@gmail.com>
2018-10-09 15:50:30 +0200

    spération du code

--- src/main/java/com/cl/AdvisorController.java
+++ src/main/java/com/cl/AdvisorController.java
@@ -1,15 +1,29 @@
 package com.cl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cl.models.Advisor;
+
 @RestController
 public class AdvisorController {
 	
+	private List<Advisor> listAdvisor = new ArrayList<Advisor>();
+	
+ 
+	
 	@RequestMapping("/advisor")
-	public Advisor getAdvisor(@RequestParam(value="name", defaultValue="toto") String name) {
-		return new Advisor("1", "Albert", "Einstein");
+	public List<Advisor> getAdvisor(@RequestParam(value="name", defaultValue="toto") String name) {
+		listAdvisor = new ArrayList<Advisor>();
+		listAdvisor.add(new Advisor("1", "Albert", "Einstein", null));
+		listAdvisor.add(new Advisor("2", "Benjamin", "Franklin", null));
+		listAdvisor.add(new Advisor("3", "Marie", "Curie", null));
+		
+		return listAdvisor;
 	}
 
 }
