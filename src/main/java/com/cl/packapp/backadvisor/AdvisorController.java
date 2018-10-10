9e35ece4ec757d4846d4d47149c1532ad678892f
ben <pinel.benjamin@gmail.com>
2018-10-10 14:08:11 +0200

    add post advisor

--- src/main/java/com/cl/packapp/backadvisor/AdvisorController.java
+++ src/main/java/com/cl/packapp/backadvisor/AdvisorController.java
@@ -3,10 +3,14 @@ package com.cl.packapp.backadvisor;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.cl.packapp.backadvisor.dao.AdvisorRepository;
 import com.cl.packapp.backadvisor.model.Advisor;
@@ -22,6 +26,18 @@ public class AdvisorController {
 	public List<Advisor> getAdvisor() {
 		final List<Advisor> advisors = repository.findAll();
 		return advisors;
+	}
+	
+	@RequestMapping(value="/advisor", method=RequestMethod.POST)
+	public ResponseEntity<?> editCard(@RequestBody Advisor advisor, UriComponentsBuilder ucBuilder) {
+		
+		advisor = repository.save(advisor);
+		
+		System.out.println("---- Card " + advisor.getId() + " added");
+		
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/advisor/{id}").buildAndExpand(advisor.getId()).toUri());
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
 	}
 	
 }
